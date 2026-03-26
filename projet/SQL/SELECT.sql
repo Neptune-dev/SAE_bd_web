15d119598f71dbd953379dedaa738d06c4ec4999
Naoti3 <47009004+Naoti3@users.noreply.github.com>
2026-03-26 19:32:55 +0100

    Rapport + SELECT.sql

---
+++ projet/SQL/SELECT.sql
@@ -0,0 +1,6 @@
+SELECT id_espece FROM espece MINUS (SELECT id_espece1 FROM autoriser WHERE autorise=1 UNION SELECT id_espece2 FROM autoriser WHERE autorise=1);
+SELECT e1.id_espece FROM espece e1 WHERE NOT EXISTS (SELECT 1 FROM espece e2 WHERE NOT EXISTS ( SELECT 1 FROM autoriser a WHERE a.id_espece1=e1.id_espece AND a.id_espece2=e2.id_espece AND a.autorise=1));
+SELECT id_personnel FROM soin WHERE RFID='RFID0006';
+SELECT id_boutique, EXTRACT(YEAR FROM date_ca) AS annee, EXTRACT(MONTH FROM date_ca) AS mois, SUM(montant) AS ca_mensuel FROM gagner GROUP BY id_boutique, EXTRACT(YEAR FROM date_ca), EXTRACT(MONTH FROM date_ca) ORDER BY id_boutique, annee, mois;
+SELECT id_boutique, EXTRACT(YEAR FROM date_ca) AS annee, SUM(montant) AS ca_annuel FROM gagner GROUP BY id_boutique, EXTRACT(YEAR FROM date_ca) ORDER BY id_boutique, annee;
+SELECT personnel.nom_personnel, personnel.prenom_personnel, type_personnel.libelle_personnel FROM personnel JOIN type_personnel ON personnel.type_personnel=type_personnel.id_type ORDER BY personnel.nom_personnel, personnel.prenom_personnel;
